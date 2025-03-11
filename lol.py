c263ecc0c4dc429fe5dc0458b0c9ea6b3e442001
unknown <miroslav.egert@infis.cz>
2025-03-11 08:44:18 +0100

    idk

---
+++ lol.py
@@ -0,0 +1,26 @@
+# načtení, přetipování
+cislo = int(input("zadejte oblibene cislo"))
+# and or not
+if(cislo > 5 and cislo < 7):
+    print("cislo je vetsi nez 5 a mensi nez 7")
+elif(cislo < 5 or cislo > 7)
+    print("cislo neni vetsi nez 5 a mensi nez 7") 
+else:
+    print("cislo je mensi nez 20")
+
+if(cislo == 5 or cislo == 7):
+    print("cislo je 5 nebo 7")
+elif(cislo == 5):
+    print("cislo je 5")
+else:
+    print("cislo je 7")
+
+if not(cislo == 5):
+    print("cislo neni 5")
+
+#příkazy pro git 
+# git add *
+# git commit -m "zprava"
+# git push
+
+#Miroslav Egert
